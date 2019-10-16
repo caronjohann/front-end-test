ca051d51b44e9ccb3038816b1fb6e8314efa3a42
Johnny Caron <caronjohann@gmail.com>
2019-10-15 21:02:54 -0400

    initialized git repo

---
+++ app.js
@@ -0,0 +1,47 @@
+// Using 'cards' from data.js as mock API data
+
+let container = document.querySelector(".container");
+
+// Setting innerHTML of each card in the page container
+container.innerHTML = cards
+  //  Using map function to place data from each object in data.js
+  .map(card => {
+    return `<div class="card">
+  <div class="image">
+  <div class="cart-icon">
+  <p class="cart-text">In Cart</p>
+  </div>
+  <button class="button show-btn">Add to cart</button>
+  <img src=${card.image} class="thumbnail" alt=${card.title} />
+  </div>
+  <div class="line"></div>
+  <h2 class="title">${card.title}</h2>
+  <p class="price">${card.price}</p>
+  <div class=${card.rating}>
+    <img src="./assets/star.svg" class="star" />
+    <img src="./assets/star.svg" class="star" />
+    <img src="./assets/star.svg" class="star" />
+    <img src="./assets/star.svg" class="star" />
+    <img src="./assets/star.svg" class="star" />
+  </div>
+</div>`;
+  })
+  .join("");
+
+let allCards = document.querySelectorAll(".card");
+
+allCards.forEach(each => {
+  let image = each.querySelector(".image");
+  let button = image.querySelector(".show-btn");
+  let cart = image.querySelector(".cart-icon");
+
+  button.onclick = function() {
+    if (button.innerText === "Add to cart") {
+      button.innerText = "Remove from cart";
+      cart.style.display = "inline-block";
+    } else {
+      button.innerText = "Add to cart";
+      cart.style.display = "none";
+    }
+  };
+});
---
+++ data.js
@@ -0,0 +1,65 @@
+let cards = [
+  {
+    id: 01,
+    image: "./assets/blue-flower.png",
+    title: "Blue Flower",
+    price: "$80.00",
+    rating: "fourStars"
+  },
+  {
+    id: 02,
+    image: "./assets/orange-flower.png",
+    title: "Orange Flower",
+    price: "$17.60",
+    rating: "threeStars"
+  },
+  {
+    id: 03,
+    image: "./assets/pink-flower.png",
+    title: "Pink Flower",
+    price: "$40.00",
+    rating: "fiveStars"
+  },
+  {
+    id: 04,
+    image: "./assets/blue-flower.png",
+    title: "Blue Flower",
+    price: "$80.00",
+    rating: "fourStars"
+  },
+  {
+    id: 05,
+    image: "./assets/orange-flower.png",
+    title: "Orange Flower",
+    price: "$17.60",
+    rating: "threeStars"
+  },
+  {
+    id: 06,
+    image: "./assets/pink-flower.png",
+    title: "Pink Flower",
+    price: "$40.00",
+    rating: "fiveStars"
+  },
+  {
+    id: 07,
+    image: "./assets/blue-flower.png",
+    title: "Blue Flower",
+    price: "$80.00",
+    rating: "fourStars"
+  },
+  {
+    id: 08,
+    image: "./assets/orange-flower.png",
+    title: "Orange Flower",
+    price: "$17.60",
+    rating: "threeStars"
+  },
+  {
+    id: 09,
+    image: "./assets/pink-flower.png",
+    title: "Pink Flower",
+    price: "$40.00",
+    rating: "fiveStars"
+  }
+];
